f28dbfd5132303c292397f1a23a5ae5c43eb6481
juarosrap <juarosrap@alum.us.es>
2022-12-18 19:55:17 +0100

    Commit

---
+++ sql/populate.sql
@@ -0,0 +1,9 @@
+
+-- La clave de todos los usuarios es iissi
+INSERT INTO Users (userId, fullName, username, password) VALUES
+	(1, 'Carlos Arévalo', 'carevalo', 'pbkdf2:sha256:150000$MjN72ikE$897d960e08be9150d943c747ff6194904fd325821945ff7d7f5c1d1d08b40bbd'),
+	(2, 'Inma Hernandez', 'inmahernandez', 'pbkdf2:sha256:150000$MjN72ikE$897d960e08be9150d943c747ff6194904fd325821945ff7d7f5c1d1d08b40bbd'),
+	(3, 'Margarita Cruz', 'mcruz', 'pbkdf2:sha256:150000$MjN72ikE$897d960e08be9150d943c747ff6194904fd325821945ff7d7f5c1d1d08b40bbd'),
+  (4, 'Alfonso Márquez', 'amarquez', 'pbkdf2:sha256:150000$MjN72ikE$897d960e08be9150d943c747ff6194904fd325821945ff7d7f5c1d1d08b40bbd'),
+  (5, 'Daniel Ayala', 'dayala1', 'pbkdf2:sha256:150000$MjN72ikE$897d960e08be9150d943c747ff6194904fd325821945ff7d7f5c1d1d08b40bbd'),
+  (6, 'David Ruiz', 'druiz', 'pbkdf2:sha256:150000$MjN72ikE$897d960e08be9150d943c747ff6194904fd325821945ff7d7f5c1d1d08b40bbd');
